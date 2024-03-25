7be4ea41db15b78076001b30c46e41811a886a7a
Kansnex <114298273+Wizkid116@users.noreply.github.com>
2024-03-25 12:39:34 -0500

    Update dwmwizard.sh

--- dwmwizard.sh
+++ dwmwizard.sh
@@ -2,36 +2,40 @@
 set -e
 SRC=~/dwmwizard
 BLD=~/dwmwizard/build
+DWM=~/dwmwizard/build/dwm-6.5
+SLS=~/dwmwizard/build/slstatus-1.0
+DMU=~/dwmwizard/build/dmenu-5.3
 mkdir $BLD
 
 cd $BLD
 	wget https://dl.suckless.org/dwm/dwm-6.5.tar.gz
-	wget https://dl.suckless.org/dmenu/dmenu-5.3.tar.gz
+	wget https://dl.suckless.org/tools/dmenu-5.3.tar.gz
 	wget https://dl.suckless.org/tools/slstatus-1.0.tar.gz
 	tar xvf dwm-*.tar.gz
 	tar xvf dmenu-*.tar.gz
 	tar xvf slstatus-*.tar.gz
+	rm dwm-*.tar.gz
+	rm dmenu-*.tar.gz
+	rm slstatus-*.tar.gz
 	sudo -v
 
-cd $BLD/slstatus-*
+cd $SLS
 	echo "Building slstatus"
-	cp $SRC/slstatus $BLD/slstatus-*/config.h
+	cp $SRC/slstatus $SLS/config.h
 	sudo make clean install
 
-cd $BLD/dmenu-*
+cd $DMU
 	echo "Building dmenu"
 	sudo make clean install
 
-cd $BLD/dwm-*
+cd $DWM
 	echo "Building dwm"
-	cp $SRC/dwm $BLD/dwm-*/config.h
+	cp $SRC/dwm $DWM/config.h
 	sudo make clean install
 
 cd $BLD
-	echo "Cleaning up"
-	rm slstatus-*.tar.gz
-	rm dmenu-*.tar.gz
-	rm dwm-*.tar.gz
-	cd $SRC && cp xdefaults ~/.Xdefaults
+	echo "Configuring urxvt"
+	cd $SRC && cp urxvt ~/.Xdefaults
 
+echo "Installation complete"
 exit 0
