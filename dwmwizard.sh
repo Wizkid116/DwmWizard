18882b5f5b97466b4b5d083205930ef631d861d2
Kansnex <114298273+Wizkid116@users.noreply.github.com>
2024-04-04 07:55:04 -0500

    Update dwmwizard.sh

--- dwmwizard.sh
+++ dwmwizard.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 #use set -n for debugging
-set -e
-#defining variables
-SRC=~/DwmWizard
-BLD=$SRC/build
-DWM=$SRC/build/dwm-6.5
-SLS=$SRC/build/slstatus-1.0
-DMU=$SRC/build/dmenu-5.3
+set -n
+source deps.sh
 #checks if the build directory exists. If not, will sleep for 5 seconds then rebuilds everything
 if [ ! -d $BLD ]; then
 	mkdir $BLD
@@ -19,37 +14,31 @@ else
 fi
 #downloads and extracts programs
 cd $BLD
-	wget https://dl.suckless.org/dwm/dwm-6.5.tar.gz
-	wget https://dl.suckless.org/tools/dmenu-5.3.tar.gz
-	wget https://dl.suckless.org/tools/slstatus-1.0.tar.gz
-	tar xvf dwm-*.tar.gz
-	tar xvf dmenu-*.tar.gz
-	tar xvf slstatus-*.tar.gz
-	rm dwm-*.tar.gz
-	rm dmenu-*.tar.gz
-	rm slstatus-*.tar.gz
+	wget $DL1 && wget $DL2 && wget $DL3
+	cat *.tar.gz | tar -xvf - -i
+	cat *.tar.gz | rm
 	sudo -v
 #builds Slstatus
 cd $SLS
 	echo "Building slstatus"
 	cp $SRC/slstatus $SLS/config.h
-	sudo make clean install
+	sudo make -j$(nproc) clean install
 #builds Dmenu
 cd $DMU
 	echo "Building dmenu"
-	sudo make clean install
+	sudo make -j$(nproc) clean install
 #builds DWM
 cd $DWM
 	echo "Building dwm"
 	cp $SRC/dwm $DWM/config.h
-	sudo make clean install
-#copies config files for urxvt and LightDM to the proper places, copies startdwm script to /usr/local/bin, and makes it executable
+	sudo make -j$(nproc) clean install
+#copies config files for urxvt and LightDM to the proper places, 
+#copies startdwm script to /usr/local/bin, and makes it executable
 cd $BLD
 	echo "Configuring urxvt"
 	cd $SRC && cp urxvt ~/.Xdefaults
-  sudo cp startdwm /usr/local/bin/startdwm
-  sudo cp desktop /usr/share/xsessions/dwm.desktop
-  sudo chmod +x /usr/local/bin/startdwm
-
-  echo "Installation complete"
+	sudo cp startdwm /usr/local/bin/startdwm
+	sudo cp desktop /usr/share/xsessions/dwm.desktop
+	sudo chmod +x /usr/local/bin/startdwm
+echo "Installation complete"
 exit 0
