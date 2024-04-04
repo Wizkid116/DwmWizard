c442ec5a8a045cf62691c691279f29e488465a52
Kansnex <114298273+Wizkid116@users.noreply.github.com>
2024-04-04 07:55:35 -0500

    Add files via upload

---
+++ deps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Dependencies and variables for DwmWizard
+SRC=~/DwmWizard
+BLD=$SRC/build
+DWM=$BLD/dwm-6.5
+SLS=$BLD/slstatus-1.0
+DMU=$BLD/dmenu-5.3
+DL1=https://dl.suckless.org/dwm/dwm-6.5.tar.gz
+DL2=https://dl.suckless.org/tools/dmenu-5.3.tar.gz
+DL3=https://dl.suckless.org/tools/slstatus-1.0.tar.gz
+
